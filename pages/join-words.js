1a5f3caa9a921c7d2e6d2d1d1109b06fcdca4ebc
Alejandro González <alejandro.gonzalez@adevinta.com>
2022-12-14 18:57:32 +0100

    react way

--- pages/join-words.js
+++ pages/join-words.js
@@ -29,10 +29,23 @@ class WordElement extends React.Component {
         };
     }
 
+    handleClick(){
+        if(this.state.status==Status.Unchecked){
+            this.setState({status: Status.Pre});
+        }
+        else if(this.state.status==Status.Pre){
+            this.setState({status: Status.Unchecked});
+        }
+        this.props.onClick()
+    }
+
     render() {
-        const clazz = "bg-light";
+        let clazz = "p-3 bg-light border";
+        if (this.state.status == Status.Pre) {
+            clazz = "p-3 bg-warning border";
+        }
         return (<>
-            <div className="p-3 {clazz} border" onClick={() => this.props.onClick()}>
+            <div className={clazz} onClick={() => this.handleClick()}>
                 {this.state.element.word}
             </div>
         </>);
@@ -48,7 +61,9 @@ class Game extends React.Component {
     }
 
     handleClick(col, id) {
-        // this.s
+        const status = this.state.status.slice();
+        status[col][id] = Status.Pre;
+        this.setState({ status: status });
     }
 
     render() {
@@ -58,7 +73,7 @@ class Game extends React.Component {
                     {[0, 1].map((col) =>
                         <div className="d-grid gap-3 col" key={"c" + col}>
                             {game[0].map((item) =>
-                                <WordElement word={item} key={"w"+item.id} onClick={() => this.handleClick(col, item.id)} />
+                                <WordElement word={item} key={"w" + item.id} onClick={() => this.handleClick(col, item.id)} />
                             )}
                         </div>
                     )}
