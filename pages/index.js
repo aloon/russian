935168ff3452420b3c2c4d7584264be0a9b4c8e2
Alejandro González <alejandro.gonzalez@adevinta.com>
2022-12-17 00:59:24 +0100

    test db

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 function Home() {
   let dbPasswd = "postgres"
-  if(process.env.NODE_ENV =="production") dbPasswd = process.env.DB_PASSWD
+  if(process.env.NODE_ENV =="production") dbPasswd = process.env.DB_PASSW
   return (
     <>
     <div>{dbPasswd}</div>
