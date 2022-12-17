daf11a500a0953079dd9862099624b0867f89c95
Alejandro González <alejandro.gonzalez@adevinta.com>
2022-12-17 20:23:11 +0100

    envs

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Link from 'next/link'
 function Home() {
   let dbPasswd = "postgres"
   if(process.env.NODE_ENV == "production") {
-    dbPasswd = process.env.DB_PASSW
+    dbPasswd = process.env["DB_PASSW"]
   }
   return (
     <>
     <div>
       -{process.env.NODE_ENV}- <br />
       -{dbPasswd}-<br/>
-      -{process.env.DB_PASSW}-<br />
+      -{process.env["DB_PASSW"]}-<br />
       </div>
     <ul>
       <li>
