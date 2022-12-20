591ec0c3eff459b864c73e228c1cfaaee2f4f1a1
Alejandro González <alejandro.gonzalez@adevinta.com>
2022-12-20 19:19:39 +0100

    testing envs

--- pages/api/join-words.js
+++ pages/api/join-words.js
@@ -1,9 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const game = [
+  let dbPasswd = "postgres"
+  if(process.env.NODE_ENV == "production") {
+    dbPasswd = process.env["DB_PASSW"]
+  }
+  const game = {
+  "a": dbPasswd,
+    "b":[
     [{ "id": 10, "word": "10" }, { "id": 20, "word": "20" }, { "id": 30, "word": "30" }],
     [{ "id": 10, "word": "a" }, { "id": 20, "word": "b" }, { "id": 30, "word": "c" }]
-];
+]};
   res.status(200).json(game)
 }
--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,9 @@ function Home() {
   if(process.env.NODE_ENV == "production") {
     dbPasswd = process.env["DB_PASSW"]
   }
-console.log(process.env);
+
   return (
     <>
-    <div>
-      -{process.env.NODE_ENV}- <br />
-      -{dbPasswd}-<br/>
-      </div>
-      {/* <ul>
-        {Object.entries(process.env).map((e) => {
-         // console.log(e);
-          return (
-            <li>*</li>
-          )
-        })}
-      </ul> */}
     <ul>
       <li>
         <Link href="/join-words">Join Words</Link>
