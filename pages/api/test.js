ca8f49750dba8a3929036bd136bf505f8ab389a9
Alejandro González <alejandro.gonzalez@adevinta.com>
2023-01-04 12:05:03 +0100

    prueba, funciona?

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,5 +2,5 @@ import conn from '../../lib/db';
 
 export default function handler(req, res) {
     const url = (process.env.NODE_ENV == "production") ? process.env["SITE_URL"] : "http://localhost:3000";
-    return res.status(200).json({ url });
+    return res.status(200).json({ url: url + '/api/login' });
 }
