0ab189a9a2269d90d063efe8338013b77dd9b974
Alejandro González <alejandro.gonzalez@adevinta.com>
2023-01-12 19:14:12 +0100

    promesas

--- pages/api/join-words/categories.js
+++ pages/api/join-words/categories.js
@@ -1,40 +1,56 @@
+// excepciones: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+
 import conn from "../../../lib/db";
 
-// export default function handler(req, res) {
-//     const jwtPassw = (process.env.NODE_ENV == "production") ? process.env["JWT_PASSW"] : "XXX";
-//     const jwt = require('jsonwebtoken');
-//     const decoded = jwt.verify(req.headers.token, jwtPassw);
-//     const queryUser = `select * from users where id=$1`;
-//     conn.query(queryUser, [decoded.userId], (err, result) => {
-//         if (result.rows.length == 0) {
-//             return res.status(401).json({ status: "error", message: "Wrong password" });
-//         } else {
-//             const queryAllCats = `select * from categories `;
-//             conn.query(queryAllCats, (err, result) => {
-//                 return res.status(200).json(result.rows.map((w) => { return { "id": w.id, "word": w.name } }));
-//             })
-//         }
-//     });
-// }
+class AuthError extends Error {
+    constructor(message, options) {
+        super(message, options);
+    }
+}
 
+const Role = {
+    Gamer: 0,
+    Admin: 1
+}
 
+async function getUserId(token) {
+    return new Promise((resolve, rejection) => {
+        const jwt = require('jsonwebtoken');
+        const jwtPassw = (process.env.NODE_ENV == "production") ? process.env["JWT_PASSW"] : "XXX";
+        jwt.verify(token, jwtPassw, function (err, decoded) {
+            if (err) {
+                rejection(new AuthError("Wrong token", { status: 401 }));
+            }
+            resolve(decoded.userId);
+        });
+    });
+}
 
-
-export default function handler(req, res) {
-    const jwtPassw = (process.env.NODE_ENV == "production") ? process.env["JWT_PASSW"] : "XXX";
-    const jwt = require('jsonwebtoken');
-    const decoded = jwt.verify(req.headers.token, jwtPassw);
+async function checkAuth(token, role) {
+    const userId = await getUserId(token);
     const queryUser = `select * from users where id=$1`;
-    conn.query(queryUser, [decoded.userId])
+    return conn.query(queryUser, [userId])
         .then(result => {
             if (result.rows.length == 0) {
-                return res.status(401).json({ status: "error", message: "Wrong password" });
+                throw new AuthError("Wrong password", { status: 401 });
             } else {
-                const queryAllCats = `select * from categories `;
-                conn.query(queryAllCats)
-                    .then(result => {
-                        return res.status(200).json(result.rows.map((w) => { return { "id": w.id, "word": w.name } }));
-                    })
+                const user_type_id = result.rows[0].user_type_id;
+                if (user_type_id < role) {
+                    throw new AuthError("Wrong role", { status: 401 });
+                }
             }
         })
+}
+
+export default function handler(req, res) {
+    const accessTo = Role.Admin;
+    checkAuth(req.headers.token, accessTo)
+        .then(() => conn.query("select * from categories"))
+        .then(result => res.status(200).json(result.rows.map((w) => { return { "id": w.id, "word": w.name } })))
+        .catch(err => {
+            if (err instanceof AuthError) {
+                return res.status(401).json({ error: err.message });
+            }
+            return res.status(500).json({ error: err.message });
+        });
 }
