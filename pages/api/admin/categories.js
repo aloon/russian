914724ccc470193585bc0cc42523a4c9f7761ff2
Alejandro González <alejandro.gonzalez@adevinta.com>
2023-01-13 09:54:41 +0100

    mejorando codigo

--- pages/api/admin/categories.js
+++ pages/api/admin/categories.js
@@ -1,49 +1,28 @@
 import conn from "../../../lib/db";
+import { getAuthUserId, AuthError } from "../../../lib/auth";
+import { Role } from "../../../lib/constants";
 
 export default function handler(req, res) {
-    const token = req.headers.token;
-    const jwt = require('jsonwebtoken');
-    let jwtPassw = (process.env.NODE_ENV == "production") ? process.env["JWT_PASSW"] : "XXX"
-    jwt.verify(token, jwtPassw, function (err, decoded) {
-        if (err) {
-            return res.status(401).json({ status: "error", message: "Wrong token" });
-        }
-        const userId = decoded.userId;
-        const query = `select * from users where id=$1`;
-        conn.query(query, [userId], (err, result) => {
-            if (result.rows.length == 0) {
-                return res.status(401).json({ status: "error", message: "Wrong token" });
-            } else {
-                if (result.rows[0].user_type_id != 1) {
-                    return res.status(401).json({ status: "error", message: "Wrong token" });
-                }
-            }
-        });
-    });
-
+    const accessTo = Role.Admin;
     const queryAllCats = `select c.id,c.name, sum(case when jw.category_id is null then 0 else 1 end) as counts
-    from categories as c
-    left join join_words as jw 
-    on c.id=jw.category_id
-    group by c.id,c.name 
-    order by c.id `;
-    if (req.method == "POST") {
-        const query = `insert into categories (name) values ($1)`;
-        conn.query(query, [req.body.word], (err, result) => {
-            if (err) {
-                console.error(err);
-                return;
+from categories as c
+left join join_words as jw 
+on c.id=jw.category_id
+group by c.id,c.name 
+order by c.id `;
+    getAuthUserId(req.headers.token, accessTo)
+        .then(() => {
+            if (req.method == "POST") {
+                const query = `insert into categories (name) values ($1)`;
+                return conn.query(query, [req.body.word])
             }
-            conn.query(queryAllCats, (err, result) => {
-                return res.status(200).json(result.rows.map((w) => {return {"id": w.id, "word":w.name, "counts":w.counts}}));
-            })
+            return Promise.resolve();
+        }).then(() => conn.query(queryAllCats))
+        .then(result => res.status(200).json(result.rows.map((w) => { return { "id": w.id, "word": w.name, "counts": w.counts } })))
+        .catch(err => {
+            if (err instanceof AuthError) {
+                return res.status(401).json({ error: err.message });
+            }
+            return res.status(500).json({ error: err.message });
         });
-    } else if (req.method == "GET") {
-        conn.query(queryAllCats, (err, result) => {
-            return res.status(200).json(result.rows.map((w) => {return {"id": w.id, "word":w.name, "counts":w.counts}}));
-        })
-    }
-    else {
-        res.status(405).json({ message: "Method not allowed" });
-    }
 }
