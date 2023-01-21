50a41ae0fcac9921d4b89fd8d80a2b88d8b42025
Alejandro González <alejandro.gonzalez@adevinta.com>
2023-01-21 13:02:30 +0100

    migrate

--- sql-migrations/003_create_table_users.sql
+++ sql-migrations/003_create_table_users.sql
@@ -4,7 +4,3 @@ CREATE TABLE users (
   passwd           VARCHAR(255) NOT NULL,
   user_type_id     INTEGER NOT NULL default 0
 );
-
-insert into users (email, passwd, user_type_id) values ('aloon.com@gmail.com', md5('moqueta..'), 1);
-insert into users (email, passwd, user_type_id) values ('elenamanukhina@gmail.com', md5('alexandra'), 1);
-insert into users (email, passwd, user_type_id) values ('test@test.test', md5('test'), 0);
