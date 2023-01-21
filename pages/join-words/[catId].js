563f0f83bf741e46cbb4af788d7b010bc43bdc28
Alejandro González <alejandro.gonzalez@adevinta.com>
2023-01-21 12:49:29 +0100

    reload component

--- pages/join-words/[catId].js
+++ pages/join-words/[catId].js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { useRouter } from 'next/router'
 import { JoinWordsStatus, url_site } from '../../lib/constants';
 import Back from '../../lib/back';
+import { Reload } from '../../lib/reload';
 
 export async function getServerSideProps(context) {
     const { catId } = context.params;
@@ -174,10 +175,7 @@ export default function JoinWords() {
                 <div className="d-flex justify-content-between bd-highlight mb-2">
                     <div className="p-2 bd-highlight"><Back href={"/join-words/categories"} /></div>
                     <div className="btn btn-outline-primary p-3 bd-highlight m-3" onClick={reload}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
-                            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
-                        </svg>
+                        <Reload width="20" height="20" />
                     </div>
                 </div>
                 <Title value={"Join Words"} />
